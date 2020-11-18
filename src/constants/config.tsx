f0735402d642fb8797f1445d15b2a77e730ce980
Nia RAMAROSANDY <ramarosandynia@gmail.com>
2020-11-18 14:18:34 +0100

    Move configs out of the Detail component

--- src/constants/config.tsx
+++ src/constants/config.tsx
@@ -1,25 +1,52 @@
 import React from 'react'
 
+import FilmDetail from '../components/details/FilmDetail'
 import FullFilmInfos from '../components/details/infos/FullFilmInfos'
 import FullPersonInfos from '../components/details/infos/FullPersonInfos'
 import FullPlanetInfos from '../components/details/infos/FullPlanetInfos'
 import FullSpeciesInfos from '../components/details/infos/FullSpeciesInfos'
 import FullStarshipInfos from '../components/details/infos/FullStarshipInfos'
 import FullVehicleInfos from '../components/details/infos/FullVehicleInfos'
+import PersonDetail from '../components/details/PersonDetail'
+import PlanetDetail from '../components/details/PlanetDetail'
 import ShortFilmInfos from '../components/details/infos/ShortFilmInfos'
 import ShortPersonInfos from '../components/details/infos/ShortPersonInfos'
 import ShortPlanetInfos from '../components/details/infos/ShortPlanetInfos'
 import ShortSpeciesInfos from '../components/details/infos/ShortSpeciesInfos'
 import ShortStarshipInfos from '../components/details/infos/ShortStarshipInfos'
 import ShortVehicleInfos from '../components/details/infos/ShortVehicleInfos'
-import { FilmInfos } from '../types/film'
-import { PersonInfos } from '../types/person'
-import { PlanetInfos } from '../types/planet'
+import SpeciesDetail from '../components/details/SpeciesDetail'
+import StarshipDetail from '../components/details/StarshipDetail'
+import VehicleDetail from '../components/details/VehicleDetail'
+import { FilmInfos, FullFilm } from '../types/film'
+import { FullPerson, PersonInfos } from '../types/person'
+import { FullPlanet, PlanetInfos } from '../types/planet'
+import { FullSpecies, SpeciesInfos } from '../types/species'
+import { FullStarship, StarshipInfos } from '../types/starship'
+import { FullVehicle, VehicleInfos } from '../types/vehicle'
 import { RESSOURCES } from './search'
-import { SpeciesInfos } from '../types/species'
-import { StarshipInfos } from '../types/starship'
-import { SwapiInfos } from '../types/search'
-import { VehicleInfos } from '../types/vehicle'
+import { SwapiDetail, SwapiInfos } from '../types/search'
+
+export const RESSOURCE_DETAIL_CONFIGS = {
+    [RESSOURCES.FILMS]: (film: SwapiDetail) => (
+        <FilmDetail film={film as FullFilm} />
+    ),
+    [RESSOURCES.PEOPLE]: (person: SwapiDetail) => (
+        <PersonDetail person={person as FullPerson} />
+    ),
+    [RESSOURCES.PLANETS]: (planet: SwapiDetail) => (
+        <PlanetDetail planet={planet as FullPlanet} />
+    ),
+    [RESSOURCES.SPECIES]: (species: SwapiDetail) => (
+        <SpeciesDetail species={species as FullSpecies} />
+    ),
+    [RESSOURCES.STARSHIPS]: (starship: SwapiDetail) => (
+        <StarshipDetail starship={starship as FullStarship} />
+    ),
+    [RESSOURCES.VEHICLES]: (vehicle: SwapiDetail) => (
+        <VehicleDetail vehicle={vehicle as FullVehicle} />
+    )
+}
 
 export const RESSOURCE_COMPONENT_CONFIGS = {
     [RESSOURCES.FILMS]: (film: SwapiInfos, showFull = false) =>
--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -5,44 +5,13 @@ import { motion } from 'framer-motion'
 import Button from '../components/shared/Button'
 import CenteredLoader from '../components/shared/CenteredLoader'
 import Error from '../components/shared/Error'
-import FilmDetail from '../components/details/FilmDetail'
 import MaterialIcon from '../components/shared/MaterialIcon'
-import PersonDetail from '../components/details/PersonDetail'
-import PlanetDetail from '../components/details/PlanetDetail'
-import SpeciesDetail from '../components/details/SpeciesDetail'
-import StarshipDetail from '../components/details/StarshipDetail'
-import VehicleDetail from '../components/details/VehicleDetail'
-import styles from './DetailPage.module.scss'
-import { FullFilm } from '../types/film'
-import { FullPerson } from '../types/person'
-import { FullPlanet } from '../types/planet'
-import { FullSpecies } from '../types/species'
-import { FullStarship } from '../types/starship'
-import { FullVehicle } from '../types/vehicle'
 import { RESSOURCES } from '../constants/search'
+import { RESSOURCE_DETAIL_CONFIGS } from '../constants/config'
 import { SwapiDetail } from '../types/search'
 import { getDetailAPI } from '../service/api'
 
-const detailMap = {
-    [RESSOURCES.FILMS]: (film: SwapiDetail) => (
-        <FilmDetail film={film as FullFilm} />
-    ),
-    [RESSOURCES.PEOPLE]: (person: SwapiDetail) => (
-        <PersonDetail person={person as FullPerson} />
-    ),
-    [RESSOURCES.PLANETS]: (planet: SwapiDetail) => (
-        <PlanetDetail planet={planet as FullPlanet} />
-    ),
-    [RESSOURCES.SPECIES]: (species: SwapiDetail) => (
-        <SpeciesDetail species={species as FullSpecies} />
-    ),
-    [RESSOURCES.STARSHIPS]: (starship: SwapiDetail) => (
-        <StarshipDetail starship={starship as FullStarship} />
-    ),
-    [RESSOURCES.VEHICLES]: (vehicle: SwapiDetail) => (
-        <VehicleDetail vehicle={vehicle as FullVehicle} />
-    )
-}
+import styles from './DetailPage.module.scss'
 
 export default function DetailPage() {
     const { type, id } = useParams<{ id: string; type: string }>()
@@ -94,7 +63,7 @@ export default function DetailPage() {
                         animate={{ opacity: 1 }}
                         transition={{ duration: 1 }}
                     >
-                        {detailMap[type as RESSOURCES](data)}
+                        {RESSOURCE_DETAIL_CONFIGS[type as RESSOURCES](data)}
                     </motion.div>
                 )
             }
